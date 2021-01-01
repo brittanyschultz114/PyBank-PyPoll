e4decb70ceb544eddc068d56020c30d1f1b9c764
unknown <brittanyschultz114@gmail.com>
2021-01-01 12:30:21 -0500

    Added another main

--- PyBank/main.py
+++ PyBank/main.py
@@ -39,8 +39,31 @@ with open(path) as budget:
         previous_row = int(row[1])
         counter += 1
 
+
+
     print("Total Months: ", str(total_month))
     print("Total: $", str(net_total))
     print("Average Change: $", str(round(sum(change_profit)/len(change_profit),2)))
     print("Greatest Increase in Profits: ", max_change_date, "$ ", max(change_profit))
     print("Greatest Decrease in Profits: ", min_change_date, "$ " , min(change_profit))
+
+
+output_file = os.path.join('..', 'analysis', 'analysis.txt')
+
+
+with open(output_file, "w", newline='') as txt_file:
+    txt_file.write('Financial Analysis')
+    txt_file.write('\n')
+    txt_file.write('------------------------------------')
+    txt_file.write("\n")
+    txt_file.write("Total Months: " + str(total_month))
+    txt_file.write("\n")
+    txt_file.write("Total: $" + str(net_total))
+    txt_file.write("\n")
+    txt_file.write("Average Change: $" + str(round(sum(change_profit)/len(change_profit),2)))
+    txt_file.write("\n")
+    txt_file.write("Greatest Increase in Profits: " + max_change_date + "$ " + str(max(change_profit)))
+    txt_file.write("\n")
+    txt_file.write("Greatest Decrease in Profits: " + min_change_date + "$ " + str(min(change_profit)))
+
+
